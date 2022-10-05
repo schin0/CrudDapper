0603704704eb20947dd7ce4323f92931fb1dd6c0
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-10-04 21:05:13 -0300

    Inserir métodos para acessar o banco no repositório

---
+++ CRUDDapper/Repository/Interfaces/IVeiculoRepository.cs
@@ -0,0 +1,14 @@
+﻿using CRUDDapper.Data.Model;
+using System.Collections.Generic;
+
+namespace CRUDDapper.Repository
+{
+    public interface IVeiculoRepository
+    {
+        Veiculo BuscarPorId(int veiculoId);
+        List<Veiculo> Listar(int veiculoId);
+        int Inserir(Veiculo veiculo);
+        int Atualizar(Veiculo veiculo);
+        int Excluir(int veiculoId);
+    }
+}
