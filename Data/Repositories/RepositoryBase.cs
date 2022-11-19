d4a2fcf05dbdc5698ffcf75b4ab98f6a3ef07103
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-11-18 21:00:53 -0300

    Criar Repositories

---
+++ Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace Data.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        public RepositoryBase(IDbConnection _connection, IConfiguration _configuration)
+        {
+            _connection = connection;
+            _configuration = configuration;
+        }
+
+        public IDbConnection connection { get;  }
+        protected IConfiguration configuration { get; }
+    }
+}
---
+++ Data/Repositories/Veiculo/VeiculoRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Interfaces.Veiculo;
+using Domain.Models.Veiculo;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+
+namespace Data.Repositories
+{
+    public class VeiculoRepository : RepositoryBase, IVeiculoRepository
+    {
+        public VeiculoRepository(IDbConnection connection, IConfiguration configuration) : base(connection, configuration)
+        {
+        }
+
+        public bool Delete(int id, Guid tenantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veiculo GetById(int id, Guid tenantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veiculo Insert(Veiculo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Veiculo model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
