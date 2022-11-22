fcdb39d5fffc34e085213263dbe10702b1870d64
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-11-22 20:46:50 -0300

    Criar DapperRepository provisório

---
+++ Data/Repositories/Dapper/DapperRepository.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Domain.Filters.Veiculo;
+using Domain.Interfaces.Dapper;
+using Domain.Models.Veiculo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Data.Repositories.Dapper
+{
+    public class DapperRepository : IDapperRepository
+    {
+        private IDbConnection _connection;
+
+        public DapperRepository()
+        {
+            _connection = new SqlConnection(@"oi");
+        }
+
+        public bool Delete(int id, Guid tenantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veiculo GetById(int id, Guid tenantId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Veiculo Insert(Veiculo model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Veiculo> List(FiltroVeiculo filtro)
+        {
+            return _connection.Query<Veiculo>("SELECT * FROM treinamentoVeiculo.Veiculo WHERE TenantId = @TenantId", new
+                { 
+                    filtro.TenantId
+                }).ToList();
+        }
+
+        public bool Update(Veiculo model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Interfaces/Dapper/IDapperRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Interfaces.Dapper
+{
+    public interface IDapperRepository
+    {
+        //TODO: usar type T
+        List<Models.Veiculo.Veiculo> List(Filters.Veiculo.FiltroVeiculo filtro);
+        Models.Veiculo.Veiculo GetById(int id, Guid tenantId);
+        Models.Veiculo.Veiculo Insert(Models.Veiculo.Veiculo model);
+        bool Update(Models.Veiculo.Veiculo model);
+        bool Delete(int id, Guid tenantId);
+    }
+}
