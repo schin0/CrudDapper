584e8ac9b57cce0d2cd9458819768fc79431ee5f
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-11-22 20:45:58 -0300

    Criar nova controller para teste

---
+++ CrudDapper/Controllers/oi.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces.Dapper;
+using Domain.Models.Veiculo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDapper.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class oi : Controller
+    {
+        private readonly IDapperRepository _dapper;
+
+        public oi(IDapperRepository dapper)
+        {
+            _dapper = dapper;
+        }
+
+        [HttpGet]
+        public List<Veiculo> Get([FromQuery] Domain.Filters.Veiculo.FiltroVeiculo filtro)
+        {
+            return _dapper.List(filtro);
+        }
+    }
+}
