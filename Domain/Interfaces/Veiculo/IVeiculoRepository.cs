2ce8631df0b7131a8771baffddc48d02c01a7a96
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-11-25 13:06:45 -0300

    Alterar interface implementada

--- Domain/Interfaces/Veiculo/IVeiculoRepository.cs
+++ Domain/Interfaces/Veiculo/IVeiculoRepository.cs
@@ -1,11 +1,8 @@
-﻿using Domain.Interfaces.Dapper;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Domain.Interfaces.Veiculo
 {
-    // TODO: Verificar para remover "Models.Veiculo."
-    public interface IVeiculoRepository : IDapperRepository<Models.Veiculo.Veiculo>
+    public interface IVeiculoRepository : IRepositoryBase<Models.Veiculo.Veiculo>
     {
         List<Models.Veiculo.Veiculo> List(Filters.Veiculo.FiltroVeiculo filtro);
     }
