f2b0157773347e413b51f3576c91980b547bbd9b
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-11-25 13:04:19 -0300

    Alterar para RepositoryBase

--- Domain/Interfaces/IRepositoryBase.cs
+++ Domain/Interfaces/IRepositoryBase.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domain.Interfaces
 {
-    public interface IRepositoryBase { }
+    public interface IRepositoryBase
+    {
+    }
 
     public interface IRepositoryBase<T>
     {
-        bool Update(T model);
         T GetById(int id, Guid tenantId);
-        bool Delete(int id, Guid tenantId);
         T Insert(T model);
+        bool Update(T model);
+        bool Delete(int id, Guid tenantId);
+    }
+
+    public interface IRepositoryBase<T, TFiltro> : IRepositoryBase<T>
+    {
+        List<T> List(TFiltro filter);
     }
 
     // TODO: Inserir listar com filtro 
