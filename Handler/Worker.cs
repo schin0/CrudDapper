2be705a98ebb3af537dddf358cdd898eda50d84a
Gabriel Schincariol <gabriel.schincariol@fagrontech.com.br>
2022-12-21 20:14:48 -0300

    Iniciar estrutura para consumer

--- Handler/Worker.cs
+++ Handler/Worker.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
@@ -12,14 +13,51 @@ namespace Handler
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuracao;
+        //private readonly IRabbitService _rabbitService;
+        //private readonly IVeiculoHandler _veiculoHandler;
 
-        public Worker(ILogger<Worker> logger)
+        //public Worker(ILogger<Worker> logger, IConfiguration configuracao, IRabbitService rabbitService, IVeiculoHandler veiculoHandler)
+        public Worker(ILogger<Worker> logger, IConfiguration configuracao)
         {
             _logger = logger;
+            _configuracao = configuracao;
+            //_rabbitService = rabbitService;
+            //_veiculoHandler = veiculoHandler;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuracao.GetSection("RabbitMQConfiguration:HostName").Value,
+                Password = _configuracao.GetSection("RabbitMQConfiguration:Password").Value,
+                UserName = _configuracao.GetSection("RabbitMQConfiguration:UserName").Value
+            }; ;
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                _logger.LogInformation("Mensagem recebida: {message}", message);
+            };
+
+            channel.BasicConsume(queue: "hello",
+                                 autoAck: true,
+                                 consumer);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
@@ -28,7 +66,6 @@ namespace Handler
         }
 
         // Tarefas:
-        // TODO: Subir um rabbit local
         // TODO: Criar handler no formato async
         // TODO: Criar uma camada de comunicação com o Rabbit
         // TODO: No inserir/Atualizar da API, mandar para a fila, para o handler processar
